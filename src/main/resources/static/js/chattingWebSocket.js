0161caaabd1fd040c1545fd2133458ae20fce5c2
sky-5462 <yjfengjunxuan@126.com>
2018-12-22 17:47:49 +0800

    username as subscribe id

--- src/main/resources/static/js/chattingWebSocket.js
+++ src/main/resources/static/js/chattingWebSocket.js
@@ -2,12 +2,14 @@ $(function () {
     var socket = new SockJS('/websocket');
     var stompClient = Stomp.over(socket);
     var roomName=location.search.substr(1).match('[^&]*' + 'roomName'+ '=([^&]*)')[1];
+    var userName = $('h2').text().match(/\w+$/)[0];
     stompClient.connect('', '', function (frame) {
         console.log('Connected: ' + frame);
         //用户聊天订阅
+        //用userName作为id
         stompClient.subscribe('/userChat/'+roomName, function (data) {
             $("#content").text($('#content').text()+data.body);
-        });
+        }, {id: userName});
     });
 
     $('#submit').click(function () {
