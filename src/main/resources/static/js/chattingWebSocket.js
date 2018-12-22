754f2c290c1db31c715d7581c73a358f01f0d6d7
sky-5462 <yjfengjunxuan@126.com>
2018-12-22 19:08:59 +0800

    样式做了一点，需要解决user识别问题

--- src/main/resources/static/js/chattingWebSocket.js
+++ src/main/resources/static/js/chattingWebSocket.js
@@ -1,19 +1,30 @@
 $(function () {
     var socket = new SockJS('/websocket');
     var stompClient = Stomp.over(socket);
-    var roomName=location.search.substr(1).match('[^&]*' + 'roomName'+ '=([^&]*)')[1];
+    var roomName = location.search.substr(1).match('[^&]*' + 'roomName' + '=([^&]*)')[1];
     var userName = $('h2').text().match(/\w+$/)[0];
     stompClient.connect('', '', function (frame) {
         console.log('Connected: ' + frame);
         //用户聊天订阅
         //用userName作为id
-        stompClient.subscribe('/userChat/'+roomName, function (data) {
-            $("#content").text($('#content').text()+data.body);
-        }, {id: userName});
+        stompClient.subscribe('/userChat/' + roomName, function (data) {
+            //只接受非自己的消息，左侧显示
+            if (data.headers.subscription !== userName) {
+                var displayMessage = $("<div class='message'>" + $('#message').val() + "</div>");
+                $('#content').append(displayMessage);
+            }
+        }, {
+            id: userName
+        });
     });
 
     $('#submit').click(function () {
-        stompClient.send('/app/' + roomName, {},$('#message').val());
-        $('#message').val('');
+        if ($('#message').val() !== '') {
+            stompClient.send('/app/' + roomName, {sourceUser: userName}, $('#message').val());
+            //发出消息右侧显示
+            var displayMessage = $("<div class='message'>" + $('#message').val() + "</div>");
+            $('#content').append(displayMessage);
+            $('#message').val('');
+        }
     })
 });
