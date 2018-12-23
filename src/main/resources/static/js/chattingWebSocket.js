31fb1104887a5ba6bb15704a1477321b4b559dfe
sky-5462 <yjfengjunxuan@126.com>
2018-12-23 09:38:35 +0800

    add time display

--- src/main/resources/static/js/chattingWebSocket.js
+++ src/main/resources/static/js/chattingWebSocket.js
@@ -5,17 +5,25 @@ $(function () {
     var stompClient = Stomp.over(socket);
     var roomName = location.search.substr(1).match('[^&]*' + 'roomName' + '=([^&]*)')[1];
     var userName = $('h2').text().match(/\w+$/)[0];
+    var time = new Date().setTime(0);
     stompClient.connect('', '', function (frame) {
         console.log('Connected: ' + frame);
         //用户聊天订阅
-        //用userName作为id
         stompClient.subscribe('/userChat/' + roomName, function (data) {
             //只接受非自己的消息，左侧显示
             if (data.headers.user !== userName) {
-                var html = "<div class='received'>" + 
+                //间隔大于2分钟则显示时间
+                var newTime = new Date();
+                if (newTime - time > 120000) {
+                    var timeMatch = /\d{2}:\d{2}/;
+                    var displayTime = $("<div class='time'>" + newTime.toTimeString().match(timeMatch) + "</div>");
+                    $('#content').append(displayTime);
+                }
+                time = newTime;
+
+                var html = "<div class='received'>" +
                     "<div class='receivedUser'>" + data.headers.user + "</div>" +
                     "<div class='receivedMessage'>" + data.body + "</div>";
-                //var displayMessage = $("<div class='received'>" + data.body + "</div>");
                 var displayMessage = $(html);
                 $('#content').append(displayMessage);
                 $('#content').scrollTop($('#content').prop('scrollHeight')); //收g消息后滚动到底
@@ -25,8 +33,18 @@ $(function () {
 
     $('#submit').click(function () {
         if ($('#message').val() !== '') {
-            stompClient.send('/app/' + roomName, {'user': userName}, $('#message').val());
+            stompClient.send('/app/' + roomName, {
+                'user': userName
+            }, $('#message').val());
             //发出消息右侧显示
+            //间隔大于2分钟则显示时间
+            var newTime = new Date();
+            if (newTime - time > 120000) {
+                var timeMatch = /\d{2}:\d{2}/;
+                var displayTime = $("<div class='time'>" + newTime.toTimeString().match(timeMatch) + "</div>");
+                $('#content').append(displayTime);
+            }
+            time = newTime;
             var displayMessage = $("<div class='sent'>" + $('#message').val() + "</div>");
             $('#content').append(displayMessage);
             $('#content').scrollTop($('#content').prop('scrollHeight')); //发消息后滚动到底
